742b20d5fe5a2431ef2527a7de3ed3dbe63938b0
kkviks <kkviks@gmail.com>
2020-10-31 10:51:26 +0530

    SignUp using Email without OTP

--- app/src/main/java/com/example/trackandtrigger/MainActivity.java
+++ app/src/main/java/com/example/trackandtrigger/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.trackandtrigger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+
+    private static final String TAG = "MainActivity";
+
+    public FirebaseAuth mAuth;
+
+    String input,password;
+
+    TextInputEditText INPUT,PASSWORD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         Tools.setSystemBarColor(this, R.color.white);
 
         mAuth = FirebaseAuth.getInstance();
+
+        INPUT = findViewById(R.id.signIn_input);
+        PASSWORD = findViewById(R.id.signIn_pass);
     }
 
     @Override
@@ -36,7 +53,48 @@ public class MainActivity extends AppCompatActivity {
     public void openSignUp(View view) {
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
+    }
+
+    public void signIN(View view) {
+        readInfo();
+        //Choose and Validate
+        if(false){
+
+        }
+        else{
+            //All fields are valid, sign in the user
+            mAuth.signInWithEmailAndPassword(input, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                // TODO: 31-10-2020
+                                Toast.makeText(MainActivity.this, "Login successful.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(user); send this to vastav
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Invalid Username or Password",
+                                        Toast.LENGTH_SHORT).show();
+                                // TODO: 31-10-2020
+                                //updateUI(null); send this to vastav
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+    }
+
+    public void readInfo(){
+        input = INPUT.getText().toString();
+        //Toast.makeText(this,input, Toast.LENGTH_SHORT).show();
+        password = PASSWORD.getText().toString();
+        //Toast.makeText(this,password,Toast.LENGTH_SHORT).show();
 
     }
-    
 }
--- app/src/main/java/com/example/trackandtrigger/SignUp.java
+++ app/src/main/java/com/example/trackandtrigger/SignUp.java
@@ -1,22 +1,61 @@
 package com.example.trackandtrigger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SignUp extends AppCompatActivity {
+
+    private static final String TAG = "SignUp";
+    //Navigate back to sign-in when sign-up successful
+    Intent intent;
+
+    public FirebaseAuth mAuth;
+
     private String username, phoneNumber, gmailID, userType,password;
     TextInputEditText USERNAME, PHONENUMBER, GMAILID,PASSWORD;
     Spinner USERTYPE;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Tools.setSystemBarLight(this);
+        Tools.setSystemBarColor(this, R.color.white);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        USERNAME = findViewById(R.id.reg_username);
+        PHONENUMBER = findViewById(R.id.reg_phone);
+        GMAILID = findViewById(R.id.reg_gmailId);
+        PASSWORD = findViewById(R.id.signup_pass);
+        USERTYPE = (Spinner)findViewById(R.id.singup_type);
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // TODO: 31-10-2020
+        //updateUI(currentUser); Send this to vastav
+    }
     protected void readInfo(){
 
         username = USERNAME.getText().toString();
@@ -35,32 +74,54 @@ public class SignUp extends AppCompatActivity {
         //Toast.makeText(this,userType, Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        Tools.setSystemBarLight(this);
-        Tools.setSystemBarColor(this, R.color.white);
-
-        USERNAME = findViewById(R.id.reg_username);
-        PHONENUMBER = findViewById(R.id.reg_phone);
-        GMAILID = findViewById(R.id.reg_gmailId);
-        PASSWORD = findViewById(R.id.signup_pass);
-        USERTYPE = (Spinner)findViewById(R.id.singup_type);
-
-    }
-    //USERNAME.setError("Invalid Username");
-
     public void createAccount(View view) {
         readInfo();
-        if( validateUsername()==false){
+        //Validation
+        if(!validateUsername(username)){
             USERNAME.setError("Username should be 6 to 30 characters long, containing only alphanumerics or _ , and should start with an alphabet.");
         }
-        else Toast.makeText(this,"Sab badiya!",Toast.LENGTH_SHORT).show();
+        else if(!validatePassword(password)){
+            PASSWORD.setError("Password should be atleast 6 characters long");
+        }
+        else {
+            //All fields are valid, sign up the user
+            // TODO: 31-10-2020 Register username into the database
+            mAuth.createUserWithEmailAndPassword(gmailID, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                // TODO: 31-10-2020
+                                Toast.makeText(SignUp.this, "Sign Up Successful.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(user); Send this to vastav
+                                // TODO: 31-10-2020 Delete this late
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUp.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                // TODO: 31-10-2020
+                                //updateUI(null);  Send this to vastav
+                            }
+
+                            // ...
+                        }
+                    });
+        }
 
     }
 
-    public boolean validateUsername(){
+    private void updateUI(FirebaseUser user) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public boolean validateUsername(String username){
         // TODO: 31-10-2020  Check if username is available
 
         /*
@@ -83,5 +144,13 @@ public class SignUp extends AppCompatActivity {
 
         Matcher m = p.matcher(username);
         return m.matches();
+    }
+
+    public static boolean validatePassword(String password){
+        /*
+        A password is considered valid if all the following constraints are satisfied:
+        1. Length greater than equal to 6
+         */
+        return password.length()>=6;
     }
 }
