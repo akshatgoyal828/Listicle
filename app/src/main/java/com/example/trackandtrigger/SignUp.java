2cbfb24298c4313a5e85511f5cd78c772059af61
kkviks <kkviks@gmail.com>
2020-10-31 08:50:34 +0530

    GUI fixes

--- app/src/main/java/com/example/trackandtrigger/SignUp.java
+++ app/src/main/java/com/example/trackandtrigger/SignUp.java
@@ -10,22 +10,11 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
-    private String username, phoneNumber, gmailID, userType;
-    TextInputEditText USERNAME, PHONENUMBER, GMAILID;
+    private String username, phoneNumber, gmailID, userType,password;
+    TextInputEditText USERNAME, PHONENUMBER, GMAILID,PASSWORD;
     Spinner USERTYPE;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        USERNAME = findViewById(R.id.reg_username);
-        PHONENUMBER = findViewById(R.id.reg_phone);
-        GMAILID = findViewById(R.id.reg_gmailId);
-        USERTYPE = (Spinner)findViewById(R.id.singup_type);
-    }
-
-    public void createAccount(View view) {
+    protected void readInfo(){
 
         username = USERNAME.getText().toString();
         //Toast.makeText(this,username, Toast.LENGTH_SHORT).show();
@@ -36,8 +25,30 @@ public class SignUp extends AppCompatActivity {
         gmailID = GMAILID.getText().toString();
         //Toast.makeText(this,gmailID, Toast.LENGTH_SHORT).show();
 
+        password = PASSWORD.getText().toString();
+        //Toast.makeText(this,password, Toast.LENGTH_SHORT).show();
+
         userType = USERTYPE.getSelectedItem().toString();
         //Toast.makeText(this,userType, Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Tools.setSystemBarLight(this);
+        Tools.setSystemBarColor(this, R.color.white);
+
+        USERNAME = findViewById(R.id.reg_username);
+        PHONENUMBER = findViewById(R.id.reg_phone);
+        GMAILID = findViewById(R.id.reg_gmailId);
+        PASSWORD = findViewById(R.id.signup_pass);
+        USERTYPE = (Spinner)findViewById(R.id.singup_type);
+
+    }
+
+    public void createAccount(View view) {
+        readInfo();
+        //USERNAME.setError("Invalid Username");
     }
 }
