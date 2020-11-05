c3b592a07a6ef9f34b4fb0be7dc1856d52a0f787
akshatgoyal828 <akshatgoyal828@gmail.com>
2020-11-06 04:23:26 +0530

    Ulta-Seedha update to firebase

--- app/src/main/java/com/example/trackandtrigger/Dairy.java
+++ app/src/main/java/com/example/trackandtrigger/Dairy.java
@@ -7,13 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+import com.jackandphantom.circularimageview.RoundedImage;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Dairy#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Dairy extends Fragment {
+public class Dairy extends Fragment implements View.OnClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,13 +57,62 @@ public class Dairy extends Fragment {
         return fragment;
     }
 
+    private TextView userName,currentDate,addJournalBtn;
+    private EditText title,thoughts;
+    private RoundedImage journalImage;
+    private ImageView addJournalCamera;
+    private ProgressBar progressBar;
+
+    private String currentUserId;
+    private String currentUserName;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
+    private FirebaseFirestore db= FirebaseFirestore.getInstance();
+    private StorageReference mStorageRef;
+
+    private String collectionPath;
+    private CollectionReference collectionReference= db.collection(collectionPath:"UserJournals");//give name to new collection
+
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+            firebaseAuth= FirebaseAuth.getInstance();
+            //addition
+            progressBar= progressBar.findViewById(R.id.upload_journal_progressbar);
+            userName= userName.findViewById(R.id. post_journal_username);
+            title= title.findViewById(R.id. add_journal_title);
+            thoughts= thoughts.findViewById(R.id. add_journal_thought);
+            currentDate= currentDate.findViewById(R.id.date);
+            addJournalCamera= addJournalCamera.findViewById(R.id.upload_image_with_camera);
+            addJournalBtn= addJournalBtn.findViewById(R.id.add_journalBtn);
+            journalImage= journalImage.findViewById(R.id.journal_image);
+            //get Username and id from bundle
+
+            Bundle bundle;//CHECK THIS NOT ASSIGNED
+
+            if(bundle!=null)
+            {
+                String username = bundle.getString(key:"")
+            }
+            //code for adding images
+
+            addJournalBtn.setOnClickListener(this);
+            addJournalCamera.setOnClickListener(this);
         }
+    }
+
+    private Object getIntent() {
+        return null;
     }
 
     @Override
@@ -60,5 +120,23 @@ public class Dairy extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dairy, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.add_journalBtn:
+
+                break;
+
+            case R.id.upload_image_with_camera:
+
+                break;
+
+
+        }
+
     }
 }
