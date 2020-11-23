040ad6040e4f13fa1dc5fe86d42a8051c5d7e7ea
kkviks <kkviks@gmail.com>
2020-11-23 13:15:57 +0530

    Journal Complete

--- app/src/main/java/com/example/trackandtrigger/EntryAdapter.java
+++ app/src/main/java/com/example/trackandtrigger/EntryAdapter.java
@@ -14,6 +14,7 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 public class EntryAdapter extends FirestoreRecyclerAdapter<Entry, EntryAdapter.EntryHolder> {
 
+
     public EntryAdapter(@NonNull FirestoreRecyclerOptions<Entry> options) {
         super(options);
     }
@@ -21,7 +22,7 @@ public class EntryAdapter extends FirestoreRecyclerAdapter<Entry, EntryAdapter.E
     @Override
     protected void onBindViewHolder(@NonNull EntryHolder holder, int position, @NonNull Entry model) {
         holder.textViewTitle.setText(model.getTitle());
-        //holder.textViewDescription.setText(model.getDescription());
+        holder.textViewDescription.setText(model.getDescription());
         //holder.textViewPriority.setText(String.valueOf(model.getPriority()));
     }
 
@@ -39,14 +40,15 @@ public class EntryAdapter extends FirestoreRecyclerAdapter<Entry, EntryAdapter.E
 
     class EntryHolder extends RecyclerView.ViewHolder {
         TextView textViewTitle;
-        //TextView textViewDescription;
+        TextView textViewDescription;
         //TextView textViewPriority;
 
         public EntryHolder(View itemView) {
             super(itemView);
             textViewTitle = (TextView)itemView.findViewById(R.id.text_view_title_journal);
-            //textViewDescription = itemView.findViewById(R.id.edit_text_entry);
+            textViewDescription = itemView.findViewById(R.id.text_view_description_journal);
             //textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
         }
     }
 }
