2585fd3b8c975c9d2dc06322891697f460973b6b
Ritika Garg <ritika.g20@gmail.com>
2020-11-28 15:38:22 +0530

    Daily Notification in UserType

--- app/src/main/java/com/example/trackandtrigger/UserType.java
+++ app/src/main/java/com/example/trackandtrigger/UserType.java
@@ -3,7 +3,11 @@ package com.example.trackandtrigger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +26,10 @@ public class UserType extends AppCompatActivity {
     RadioGroup radioGroup;
     RadioButton radioButton;
     TextView textView;
+    private AlarmManager alarmMgr;
+    private Context context;
+    private PendingIntent alarmIntent;
+    Button notification;
 
     TextView time;
 
@@ -29,6 +37,8 @@ public class UserType extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_user_type);
+
+            context=getApplicationContext();
             //  initiate the edit text
             time = findViewById(R.id.time);
             // perform click event listener on edit text
@@ -51,6 +61,27 @@ public class UserType extends AppCompatActivity {
                 }
             });
 
+            notification=findViewById(R.id.button2);
+
+            notification.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Set the alarm to start at approximately 2:00 p.m.
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.set(Calendar.HOUR_OF_DAY,13);
+
+
+// With setInexactRepeating(), you have to use one of the AlarmManager interval
+// constants--in this case, AlarmManager.INTERVAL_DAY.
+
+                    alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+                    Intent intent = new Intent(context, ReminderBroadcast.class);
+                    alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+                    alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                            AlarmManager.INTERVAL_DAY, alarmIntent);
+                }
+            });
 
             radioGroup = findViewById(R.id.radioGroup);
             textView = findViewById(R.id.text_view_selected);
