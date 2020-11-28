c95b7694a03ec064d54467aad113553697eef0ba
Ritika Garg <ritika.g20@gmail.com>
2020-11-28 20:41:58 +0530

    Notification Changed Again

--- app/src/main/java/com/example/trackandtrigger/ReminderBroadcast.java
+++ app/src/main/java/com/example/trackandtrigger/ReminderBroadcast.java
@@ -7,48 +7,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 public class ReminderBroadcast extends BroadcastReceiver {
 
-    private static final String CHANNEL_ID = "CHANNEL_SAMPLE";
-
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context arg0, Intent arg1) {
+        Toast.makeText(arg0, "Alarm received!", Toast.LENGTH_LONG).show();
 
-        // Get id & message
-        int notificationId = intent.getIntExtra("notificationId", 0);
-        String message = intent.getStringExtra("message");
-
-        // Call MainActivity when notification is tapped.
-        Intent mainIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
-
-        // NotificationManager
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // For API 26 and above
-            CharSequence channelName = "My Notification";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        // Prepare Notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle("TITLE")
-                .setContentText(message)
-                .setContentIntent(contentIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true);
-
-        // Notify
-        notificationManager.notify(notificationId, builder.build());
     }
 }
