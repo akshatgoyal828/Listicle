0cf3bc5bb644882ec925344f443d0da9659e43ca
kkviks <kkviks@gmail.com>
2020-11-29 10:30:59 +0530

    Links added

--- app/src/main/java/com/example/trackandtrigger/Collect.java
+++ app/src/main/java/com/example/trackandtrigger/Collect.java
@@ -19,17 +19,17 @@ public class Collect {
     public static String findURL(int image_id) {
         String url="";
         switch (image_id){
-            case 1: url="https://i.ibb.co/cLkzFTJ/ic-flipkart.png"; break;
-            case 2: url="https://i.ibb.co/bNSPC3d/UPI-Logo-PNG.png"; break;
-            case 3: url="https://i.ibb.co/sb50kzW/Paypal-Icon-979x1024.png"; break;
-            case 4: url="https://i.ibb.co/mTPRmM3/SBI-Bank-PNG-Icon-1024x1024.png"; break;
-            case 5: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
-            case 6: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
-            case 7: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
-            case 8: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
-            case 9: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
-            case 10: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
-            case 11: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
+            //case 1: url="https://i.ibb.co/cLkzFTJ/ic-flipkart.png"; break;
+            case 1: url="https://i.ibb.co/2n3xNcR/unnamed-2.png"; break;
+            case 2: url="https://i.ibb.co/BgVpd6w/unnamed-3.png"; break;
+            case 3: url="https://i.ibb.co/6BXf0JH/unnamed-4.png"; break;
+            case 4: url="https://i.ibb.co/6BXf0JH/unnamed-5.png"; break;
+            case 5: url="https://i.ibb.co/6BXf0JH/unnamed-6.png"; break;
+            case 6: url="https://i.ibb.co/6BXf0JH/unnamed-7.png"; break;
+            case 7: url="https://i.ibb.co/6BXf0JH/unnamed-8.png"; break;
+            case 8: url="https://i.ibb.co/6BXf0JH/unnamed-9.png"; break;
+            //case 10: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
+           // case 11: url="https://i.ibb.co/5x4LFVs/Axis-Bank-PNG-Logo-1024x1024.png"; break;
             default: url="https://i.ibb.co/B3469zs/no-image-found-360x260.png";
         }
         return url;
